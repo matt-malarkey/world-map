3e17b5995ef580e07f04366bcb64eec8257715bf
Matt Malarkey <mm5917@imperial.ac.uk>
2018-12-19 13:00:01 +0000

    Stop an effect properly on exit

--- src/main/java/map/world/WorldMapController.java
+++ src/main/java/map/world/WorldMapController.java
@@ -4,7 +4,7 @@ import map.world.effect.TestAll;
 import map.world.view.SimulationView;
 import map.world.view.WorldMapView;
 
-// TODO: make singleton
+// TODO: make singleton?
 public class WorldMapController {
 
   // View to update when pixels change
@@ -24,9 +24,17 @@ public class WorldMapController {
     // Set startup screen effect
     Thread effectRunner = new Thread(new TestAll(view));
 
-    // Run the effect
+    // Start the effect
     effectRunner.run();
 
+    // Run effect for 5 seconds before exiting
+    try {
+      Thread.sleep(5000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    // TODO: act on server input
 //    while (!(sa.interrupted)) {
 //      nanosleep(&effect_runner->effect->time_delta, NULL);
 //      effect_runner->effect->run(effect_runner);
@@ -49,8 +57,14 @@ public class WorldMapController {
 //      pthread_mutex_unlock((pthread_mutex_t *) &sa.mutex);
 //    }
 
+    // TODO: make effects extend Thread class and add public method to do the below operations to end.
     // Stop running the current effect and exit
     effectRunner.interrupt();
+    try {
+      effectRunner.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 
   public static void main(String[] args) {
