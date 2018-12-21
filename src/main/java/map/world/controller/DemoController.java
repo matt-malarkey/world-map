6ceff64ff28485e300448d5764fa8b1ea44b0a4b
Matt Malarkey <mm5917@imperial.ac.uk>
2018-12-21 11:41:51 +0000

    Change demo getCommand to test out play and pause commands

--- src/main/java/map/world/controller/DemoController.java
+++ src/main/java/map/world/controller/DemoController.java
@@ -6,11 +6,23 @@ public class DemoController implements WorldMapController {
 
   private int count = 0;
 
+  // Do nothing, then pause, then play, then exit.
   @Override public Optional<WorldMapCommand> getCommand() {
-    if (count < 100) {
+    if (count < 1) {
       count++;
       return Optional.empty();
     }
+
+    if (count < 10) {
+      count++;
+      return Optional.of(WorldMapCommand.PAUSE);
+    }
+
+    if (count < 11) {
+      count++;
+      return Optional.of(WorldMapCommand.PLAY);
+    }
+
     return Optional.of(WorldMapCommand.EXIT);
   }
 }
