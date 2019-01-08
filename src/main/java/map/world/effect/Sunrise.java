bb95b2a851c557863e6f2aec27aceffe170c843a
Matt Malarkey <mm5917@imperial.ac.uk>
2019-01-08 20:40:59 +0000

    Simplify sunrise class, add internal world time to update each frame

--- src/main/java/map/world/effect/Sunrise.java
+++ src/main/java/map/world/effect/Sunrise.java
@@ -3,9 +3,15 @@ package map.world.effect;
 import map.world.view.OpcPixel;
 import map.world.view.WorldMapView;
 
+import java.time.Duration;
+import java.time.LocalTime;
+
 public class Sunrise extends WorldMapEffect {
 
   private static final int FRAME_DELAY = 20;
+
+  private LocalTime internalTime = LocalTime.of(0, 0, 0);
+  private final Duration advanceTime = Duration.ofMinutes(40);
 
   public Sunrise(WorldMapView view) {
     super(view, FRAME_DELAY, true);
@@ -13,18 +19,20 @@ public class Sunrise extends WorldMapEffect {
     // TODO: Read in config file here
   }
 
-  // TODO: map to internal time class, from frame number
-  private int calculateWorldTime() {
-    return 0;
+  // TODO: lookup pixel in config map and scale white value
+  private int calculateSunlight(int pixel) {
+    return OpcPixel.makePixel(100, 100, 100);
   }
 
-  // TODO: create abstract classes for looping and non-looping effects
   /* How it works - internal time is mapped to by frame number, pixels are
      turned on if internal time is within the sun up time for that pixel */
   @Override void calculateNextFrame(int frame) {
+    // Advance internal world time by a fixed amount each frame to model daytime
+    internalTime = internalTime.plus(advanceTime);
+
+    // Update pixel lists based on new internal world time
     for (int pixel = 0; pixel < NUM_PIXELS; pixel++) {
-      int r = 0, g = 0, b = 0;
-      setPixel(pixel, OpcPixel.makePixel(r, g, b));
+      setPixel(pixel, calculateSunlight(pixel));
     }
   }
 }
